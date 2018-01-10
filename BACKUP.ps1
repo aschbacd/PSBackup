114d9e1ca670ad12d18e44b5bdd18522a0898b73
David Kröll <david.kroell@outlook.com>
2018-01-10 20:03:43 +0100

    Add folder-picker dialog

--- BACKUP.ps1
+++ BACKUP.ps1
@@ -1,6 +1,27 @@
-$sourceRoot = "\\server\share"         # folder to be backed up - no backslash on the end of path
-$destinationRoot = "E:\Backup"    # folder that holds backup - no backslash on the end of path
+Function Read-FolderBrowserDialog([string]$InitialDirectory, [string]$Description)
+{
+    Add-Type -AssemblyName System.Windows.Forms
+    $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+    $FolderBrowser.Description = $Description
+    $result = $FolderBrowser.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true }))
+    if ($result -eq [Windows.Forms.DialogResult]::OK){
+        return $FolderBrowser.SelectedPath
+    }
+    else {
+        exit
+    }
+}
 
-new-item $destinationRoot"\Backup" -itemtype directory
-Copy-Item -Path $sourceRoot"\*" -Recurse -Destination $destinationRoot"\Backup" -Container
-icacls $sourceRoot"\*" /save $destinationRoot"\ACL" /T
+
+$sourceRoot = Read-FolderBrowserDialog -InitialDirectory "Desktop" -Description "Select directory beeing backed up."
+$destinationRoot = Read-FolderBrowserDialog -InitialDirectory $sourceRoot -Description "Select directory for saving the backup."
+
+$dateStr = Get-Date -UFormat "%Y%m%d-%H%M"
+ 
+$destinationRoot += (Get-Item $sourceRoot).BaseName + "@" + $dateStr
+
+
+new-item $destinationRoot -itemtype directory
+Copy-Item -Path $sourceRoot -Recurse -Destination $destinationRoot -Container
+
+icacls $sourceRoot /save $destinationRoot"\.acls" /T
