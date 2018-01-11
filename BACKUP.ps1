2d1ca224a9d87fe495d96b7f5eca7c48da88a8e3
David Kröll <kroelld@edu.htl-villach.at>
2018-01-11 20:59:29 +0100

    add decompression and file/folder pickers to restore script

--- BACKUP.ps1
+++ BACKUP.ps1
@@ -1,16 +1,4 @@
-Function Read-FolderBrowserDialog([string]$InitialDirectory, [string]$Description)
-{
-    Add-Type -AssemblyName System.Windows.Forms
-    $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
-    $FolderBrowser.Description = $Description
-    $result = $FolderBrowser.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true }))
-    if ($result -eq [Windows.Forms.DialogResult]::OK){
-        return $FolderBrowser.SelectedPath
-    }
-    else {
-        exit
-    }
-}
+. .\lib\OpenFolderDialog.ps1
 
 #find filenames
 $sourceRoot = Read-FolderBrowserDialog -InitialDirectory "Desktop" -Description "Select directory beeing backed up."
